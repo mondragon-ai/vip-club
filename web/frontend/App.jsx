8af78265fabc4238a9499ce7d5a2f9db87462266
mondragon-ai <angel.mondragon@yahoo.com>
2022-08-11 11:43:19 -0500

    Adding pages & custom session

--- web/frontend/App.jsx
+++ web/frontend/App.jsx
@@ -21,8 +21,12 @@ export default function App() {
             <NavigationMenu
               navigationLinks={[
                 {
-                  label: "Page name",
-                  destination: "/pagename",
+                  label: "Products",
+                  destination: "/products",
+                },
+                {
+                  label: "Customers",
+                  destination: "/customers",
                 },
               ]}
             />
